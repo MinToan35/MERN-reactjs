defe122030bc15601197aabadc280715bbafc587
MinToan35 <100203431+MinToan35@users.noreply.github.com>
2022-06-21 13:15:22 +0700

    Update constants.js

--- src/context/constants.js
+++ src/context/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://mern-reactjs.vercel.app/";
+    : "https://mern-reactjs.vercel.app/auth/";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "Cluster0";
